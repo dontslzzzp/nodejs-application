2da9b6c43458a8cca9ae79005a2fd0fe4adaaa4f
dontslzzzp <156986802+dontslzzzp@users.noreply.github.com>
2024-04-07 20:56:15 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,19 +1,19 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.80.138:9000/',
+  serverUrl: 'http://172.31.84.151:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'squ_fa2efa49786e372c7bf9080dfdfbe8116e18a4af',
-	    //'sonar.login': '19d0cae4b5116d3c155c379a3a0cfda7f915f955',
+	    //'sonar.login': 'squ_fa2efa49786e372c7bf9080dfdfbe8116e18a4af',
+	    'sonar.login': '19d0cae4b5116d3c155c379a3a0cfda7f915f955',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
 	  //'sonar.tests': 'specs',
-            'sonar.inclusions' : '**'
+          //'sonar.inclusions' : '**'
        },
 }, () => {});
